d2c3883aded8f1607efb87a3c139effe8126b59a
sanvatify <sanidhya.vatsa@gmail.com>
2023-11-25 22:49:07 +0530

    Index.cshtml and _Layout.cshtml updated

--- My_Project/ETradingSystem/ETradingSystem/Controllers/HomeController.cs
+++ My_Project/ETradingSystem/ETradingSystem/Controllers/HomeController.cs
@@ -12,13 +12,20 @@ namespace ETradingSystem.Controllers
         {
             return View();
         }
-
-      
-
+ 
+        public ActionResult Register()
+        {
+            return View();
+        }
+ 
+        public ActionResult Login()
+        {
+            return View();
+        }
+ 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
