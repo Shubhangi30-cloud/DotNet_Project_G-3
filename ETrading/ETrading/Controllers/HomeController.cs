1ab309be4749b8c39e23646290659ad88606bd8b
Hannah Philipraj <hannahp@infinite.com>
2023-11-27 00:03:18 +0530

    views

--- ETrading/ETrading/Controllers/HomeController.cs
+++ ETrading/ETrading/Controllers/HomeController.cs
@@ -13,17 +13,15 @@ namespace ETrading.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Role()
+        {
             return View();
         }
     }
